a8cd45565095f143626f3f295c187904953c83a4
Matthew Woodard <matthew@goguardian.com>
2014-10-22 15:52:30 -0700

    Changed order of requires

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-exports.checkBoxTree = require('jquery.checkboxtree.min.js');
-exports.jquery_ui = require('jquery-ui-1.8.12.custom.min.js');
+exports.jquery_ui = require('./jquery-ui-1.8.12.custom.min.js');
+exports.checkBoxTree = require('./jquery.checkboxtree.min.js');
